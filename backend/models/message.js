30669340f1b2ffade6cecfb776a37130551a2ac8
Sharleen Jamoza <sharleenjamoza@gmail.com>
2025-03-16 08:44:58 +0300

    fixed message model

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,12 +1,25 @@
+// models/Message.js - Make sure this file name has the capital M
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   {
-    senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    receiverId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, required: true },
+    senderId: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "User", 
+      required: true 
+    },
+    receiverId: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "User", 
+      required: true 
+    },
+    content: { 
+      type: String, 
+      required: true 
+    },
   },
   { timestamps: true }
 );
 
+// Make sure to export correctly
 module.exports = mongoose.model("Message", messageSchema);
